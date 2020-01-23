f34fdeed9ceebd3e165c04cc27d2b666ede92c4e
hasashin <dominik.hazak@gmail.com>
2020-01-23 08:08:53 +0100

    Initial Commit

---
+++ Habitica/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Habitica
+//
+//  Created by Dominik Hażak on 23/01/2020.
+//  Copyright © 2020 Dominik Hażak. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
